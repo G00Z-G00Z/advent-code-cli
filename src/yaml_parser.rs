408add1b99b032a7b9dc3598607bfcbad91bc554
G00Z_G00Z <edygomezgg4@gmail.com>
2023-06-14 21:14:28 -0400

    Wrote tests for templating

--- src/yaml_parser.rs
+++ src/yaml_parser.rs
@@ -37,3 +37,102 @@ pub fn parse_yml_file(
 
     Ok(rendered)
 }
+
+#[cfg(test)]
+mod tests {
+
+    use std::fmt::format;
+
+    use super::*;
+
+    #[test]
+    fn test_parse_yml_file_without_variables() {
+        let metadata = DayChallenge::new(1, 2020, "Rust".to_string(), "test-2-numbers".into());
+
+        let file_content = "
+rust:
+  commands:
+    - cargo new hello
+       
+  files:
+    - name: .env
+      content: |
+         DEMO_APP=1
+    - src/lib.rs
+    - input.txt
+    - demo-input.txt
+
+  folders: 
+    - docs/
+";
+
+        let expected = "
+rust:
+  commands:
+    - cargo new hello
+       
+  files:
+    - name: .env
+      content: |
+         DEMO_APP=1
+    - src/lib.rs
+    - input.txt
+    - demo-input.txt
+
+  folders: 
+    - docs/
+";
+
+        let parsed_content = parse_yml_file(&file_content, &metadata).unwrap();
+
+        assert_eq!(&parsed_content, &expected);
+    }
+
+    #[test]
+    fn test_parse_yml_file_with_variables() {
+        let metadata = DayChallenge::new(1, 2020, "Rust".to_string(), "test-2-numbers".into());
+
+        let file_content = "
+rust:
+  commands:
+    - cargo new {{title}}-{{year}}-{{day}}-{{language}}
+       
+  files:
+    - name: .env
+      content: |
+         DEMO_APP=1
+    - src/lib.rs
+    - input.txt
+    - demo-input.txt
+    - notes-day-{{day}}.txt
+
+  folders: 
+    - docs/
+";
+
+        let expected = format!(
+            "
+rust:
+  commands:
+    - cargo new {}-{}-{}-{}
+       
+  files:
+    - name: .env
+      content: |
+         DEMO_APP=1
+    - src/lib.rs
+    - input.txt
+    - demo-input.txt
+    - notes-day-{}.txt
+
+  folders: 
+    - docs/
+",
+            &metadata.title, &metadata.year, &metadata.day, &metadata.language, &metadata.day
+        );
+
+        let parsed_content = parse_yml_file(&file_content, &metadata).unwrap();
+
+        assert_eq!(&parsed_content, &expected);
+    }
+}
