1889d83a37a6fdb44f29d087bae681b3969a3abb
G00Z_G00Z <edygomezgg4@gmail.com>
2023-06-14 20:31:41 -0400

    Yaml Parser Function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod interfaces;
 pub mod yaml_parser;
 
 pub fn hi() {
--- src/yaml_parser.rs
+++ src/yaml_parser.rs
@@ -1,3 +1,41 @@
-pub fn parse_yaml() {
-    println!("Parsing yaml");
+use std::{collections::HashMap, fs};
+use tera::Tera;
+
+use crate::interfaces::DayChallenge;
+
+/// Error type for the yaml parser
+#[derive(Debug)]
+pub enum YamlParserError {
+    IoError(std::io::Error),
+    TeraError(tera::Error),
+}
+
+/// Parses the yml file with the given metadata
+pub fn parse_yml_file(
+    yml_file: &str,
+    day_challenge: &DayChallenge,
+) -> Result<String, YamlParserError> {
+    let content = fs::read_to_string(yml_file).map_err(|e| YamlParserError::IoError(e))?;
+    let mut tera = Tera::default();
+
+    let mut context = tera::Context::new();
+
+    // Project metadata
+    let user_provided_vars = [
+        ("day", &day_challenge.day.to_string()),
+        ("year", &day_challenge.year.to_string()),
+        ("language", &day_challenge.language.to_string()),
+        ("title", &day_challenge.title.to_string()),
+    ];
+
+    // Insert the metadata into the context
+    for (key, value) in user_provided_vars.iter() {
+        context.insert(*key, *value);
+    }
+
+    let rendered = tera
+        .render_str(&content, &context)
+        .map_err(|e| YamlParserError::TeraError(e))?;
+
+    Ok(rendered)
 }
