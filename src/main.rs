19179c6b952a63dd8ec11e2e50ef7c0f5352cfb4
G00Z_G00Z <edygomezgg4@gmail.com>
2023-06-14 21:06:03 -0400

    Updated example

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use advent_code_cli::{interfaces::DayChallenge, yaml_parser};
 
-const template_name: &str = "template.yml";
+const TEMPLATE_NAME: &str = "template.yml";
+
 fn main() {
+    let content =
+        std::fs::read_to_string(TEMPLATE_NAME).expect("Something went wrong reading the file");
     let metadata = DayChallenge::new(1, 2020, "Rust".to_string(), "martin".to_string());
 
     let parsed_content =
-        yaml_parser::parse_yml_file(template_name, &metadata).expect("Problem with parsing");
+        yaml_parser::parse_yml_file(&content, &metadata).expect("Problem with parsing");
 
     println!("{}", parsed_content)
 }
