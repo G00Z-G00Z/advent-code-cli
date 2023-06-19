8616fde4583a4c2b87fd13f8338185d723284f72
G00Z_G00Z <edygomezgg4@gmail.com>
2023-06-19 15:09:17 -0400

    Refactored code

--- src/file_handler.rs
+++ src/file_handler.rs
@@ -1,9 +1,9 @@
-use std::fs::File;
-use std::io::Write;
-use std::path::{Path, PathBuf};
-use tempdir::TempDir;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
-use crate::interfaces::ProgrammingTemplate;
+use crate::interfaces::DayChallenge;
 
 #[derive(Debug)]
 pub enum BuildError {
@@ -12,76 +12,71 @@ pub enum BuildError {
     DirectoryError,
 }
 
-const FIRST_YEAR: u32 = 2015;
-
+/// Hanldes the creation of the directory structure for the advent of code
 pub struct AventStructure {
     base_directory: PathBuf,
 }
 
 impl AventStructure {
-    pub fn new(base_directory: &str) -> AventStructure {
+    pub fn new(base_directory: &Path) -> AventStructure {
         AventStructure {
             base_directory: PathBuf::from(base_directory),
         }
     }
 
-    pub fn initialize_year(&mut self, year: u32) -> Result<(), BuildError> {
-        // Creates a folder structure fot the project
-        // Creates a src folder
-        // Creates folders made by years
-
-        // Creates a src folder
-        let src_path = self.base_directory.join("src");
-
-        // Creates folders made by years
-        let year_path = src_path.join(year.to_string());
-
-        Ok(())
+    /// Creates the directory of a day challenge structure for the advent of code
+    pub fn add_day(&mut self, challenge: &DayChallenge) -> Result<PathBuf, BuildError> {
+        let year_path = self
+            .base_directory
+            .join("src")
+            .join(challenge.year.to_string())
+            .join(format!("Day-{:02}-{}", challenge.day, challenge.title))
+            .join(challenge.language.to_string());
+        fs::create_dir_all(&year_path).map_err(|_| BuildError::DirectoryError)?;
+        Ok(year_path)
     }
-}
-
-pub fn build_template(
-    template: &ProgrammingTemplate,
-    base_directory: &str,
-) -> Result<(), BuildError> {
-    // Make the base directory a path instance
-    let base_directory = PathBuf::from(base_directory);
-
-    unimplemented!()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::io::Read;
+
+    use tempdir::TempDir;
 
     fn get_tmp_dir() -> TempDir {
         TempDir::new("testing_cli").unwrap()
     }
 
+    fn get_challenge() -> DayChallenge {
+        DayChallenge {
+            day: 1,
+            year: 2015,
+            language: "rust".to_string(),
+            title: "test-my-challenge".to_string(),
+        }
+    }
+
     #[test]
-    fn test_create_file() {
-        // Create a temporary directory
-        let temp_dir = get_tmp_dir();
-        println!("The directory is located at {:?}", temp_dir.path());
-        let base_dir = temp_dir.path();
+    fn test_initialize_year_folder_correctly() {
+        let base_dir = get_tmp_dir();
+        let challenge = get_challenge();
+        const YEAR: u32 = 2015;
+        let mut structure = AventStructure::new(base_dir.as_ref());
 
-        // Define the file path within the base directory
-        let file_path: PathBuf = base_dir.join("my_file.txt");
+        let new_dir = structure.add_day(&challenge).unwrap();
 
-        // Open the file and write some content
-        let mut file = File::create(&file_path).unwrap();
-        file.write_all(b"Hello, World!").unwrap();
+        let challenge_path = base_dir
+            .path()
+            .join("src")
+            .join(YEAR.to_string())
+            .join(format!("Day-{:02}-{}", challenge.day, challenge.title))
+            .join(challenge.language.to_string());
 
-        // Assert that the file exists
-        assert!(file_path.exists());
-
-        // Read and verify the file content
-        let mut file_content = String::new();
-        let mut file = File::open(&file_path).unwrap();
-        file.read_to_string(&mut file_content).unwrap();
-        assert_eq!(file_content, "Hello, World!");
-
-        // Additional assertions or tests can be added here
+        assert!(
+            challenge_path.exists(),
+            "Year {:?} directory was not created",
+            challenge_path
+        );
+        assert_eq!(challenge_path, new_dir);
     }
 }
