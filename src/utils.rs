64b2bdcf33d4d495cb848f2b2facd4c5d103d8cd
G00Z_G00Z <edygomezgg4@gmail.com>
2023-06-28 20:46:22 -0400

    Added function to prompt user to cancel

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use dialoguer::Confirm;
 use std::path::PathBuf;
 
 /// Lists the folders in the directory
@@ -19,4 +20,30 @@ pub fn list_folder_names(path: &PathBuf) {
         }
     }
     return;
+}
+
+/// Prompts user to remove a directory
+/// If force is true, it will remove it without prompting
+pub fn prompt_to_remove_directory(path: &PathBuf, force: bool) {
+    if force {
+        std::fs::remove_dir_all(path).unwrap();
+        println!("Removed {}", path.display());
+        return;
+    }
+
+    let confirmed = Confirm::new()
+        .with_prompt(format!(
+            "Are you sure you want to delete {}?",
+            path.display()
+        ))
+        .interact()
+        .expect("Canceling operation");
+
+    if confirmed {
+        // Proceed with the desired action
+        std::fs::remove_dir_all(path).unwrap();
+        println!("Removed {}", path.display());
+    } else {
+        println!("Canceling operation");
+    }
 }
