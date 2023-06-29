8ec154128de226328504afebb4ae63cb12a216ca
G00Z_G00Z <edygomezgg4@gmail.com>
2023-06-28 20:25:54 -0400

    List command ready

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use advent_code_cli::{
-    cli::Cli,
-    interfaces::DayChallenge,
-    yaml_parser::{self, parse_values_yml},
-};
+use std::fs;
+
+use advent_code_cli::cli::{Cli, Commands};
 use clap::Parser;
 
 const TEMPLATE_NAME: &str = "template.yml";
@@ -29,5 +27,72 @@ fn main() {
     if !base_directory.exists() {
         eprintln!("Base directory does not exist. Please select a new base directory");
         return;
+    }
+
+    let base_directory = base_directory.join("src");
+    fs::create_dir_all(&base_directory).unwrap();
+
+    match cli.command {
+        Commands::List { year } => {
+            if year.is_none() {
+                // List all the years in the base directory
+                println!("Listing all the years in the base directory: ");
+                for entry in std::fs::read_dir(&base_directory).unwrap() {
+                    let entry = entry.unwrap();
+                    let path = entry.path();
+                    if path.is_dir() {
+                        println!(
+                            "- {}",
+                            path.components()
+                                .last()
+                                .unwrap()
+                                .as_os_str()
+                                .to_str()
+                                .unwrap()
+                        );
+                    }
+                }
+                return;
+            }
+
+            let year = year.unwrap();
+
+            let year_path = base_directory.join(year.to_string());
+
+            if !year_path.exists() {
+                println!("You do not have entries for the year {}...", year);
+                return;
+            }
+
+            // List all the days in the year
+            println!("Listing all the days in the year: {}", year);
+
+            for entry in std::fs::read_dir(&year_path).unwrap() {
+                let entry = entry.unwrap();
+                let path = entry.path();
+                if path.is_dir() {
+                    println!(
+                        "- {}",
+                        path.components()
+                            .last()
+                            .unwrap()
+                            .as_os_str()
+                            .to_str()
+                            .unwrap()
+                    );
+                }
+            }
+        }
+        Commands::Add {
+            day,
+            year,
+            language,
+            title,
+        } => todo!(),
+        Commands::Remove {
+            day,
+            year,
+            language,
+        } => todo!(),
     }
 }
