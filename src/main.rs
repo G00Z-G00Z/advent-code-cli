ef6e1e9e127977047d05942bacd5e7c186afb5fa
G00Z_G00Z <edygomezgg4@gmail.com>
2023-06-28 20:12:06 -0400

    Initialize the directories

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,19 @@ fn main() {
         .template_file
         .unwrap_or_else(|| std::path::PathBuf::from("./template.yml"));
 
+    // Check if the template file exists
+    if !template_file.exists() {
+        eprintln!("Template file does not exist. Create a new one to continue");
+        return;
+    }
+
     let base_directory = cli
         .base_directory
         .unwrap_or_else(|| std::path::PathBuf::from("./"));
 
-    println!("{:?}", base_directory);
+    // Check if the base directory exists
+    if !base_directory.exists() {
+        eprintln!("Base directory does not exist. Please select a new base directory");
+        return;
+    }
 }
